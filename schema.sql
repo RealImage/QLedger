ab42a79a3c5d31111793341a4a347159fde29f9c
Sudhir Jonathan <sudhir.j@gmail.com>
2017-04-13 00:43:41 +0530

    Removed personal references

--- schema.sql
+++ schema.sql
@@ -16,51 +16,43 @@ CREATE TABLE account_tags (
     key character varying NOT NULL,
     value character varying
 );
-ALTER TABLE account_tags OWNER TO sudhir;
 CREATE TABLE accounts (
     id character varying NOT NULL,
     data jsonb DEFAULT '{}'::jsonb NOT NULL
 );
-ALTER TABLE accounts OWNER TO sudhir;
 CREATE TABLE lines (
     id bigint NOT NULL,
     transaction_id character varying NOT NULL,
     account_id character varying NOT NULL,
     delta integer NOT NULL
 );
-ALTER TABLE lines OWNER TO sudhir;
 CREATE VIEW current_balances AS
  SELECT lines.account_id,
     sum(lines.delta) AS balance
    FROM lines
   GROUP BY lines.account_id;
-ALTER TABLE current_balances OWNER TO sudhir;
 CREATE VIEW invalid_transactions AS
  SELECT lines.transaction_id,
     sum(lines.delta) AS sum
    FROM lines
   GROUP BY lines.transaction_id
  HAVING (sum(lines.delta) > 0);
-ALTER TABLE invalid_transactions OWNER TO sudhir;
 CREATE SEQUENCE lines_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-ALTER TABLE lines_id_seq OWNER TO sudhir;
 ALTER SEQUENCE lines_id_seq OWNED BY lines.id;
 CREATE TABLE transaction_tags (
     transaction_id character varying NOT NULL,
     key character varying NOT NULL,
     value character varying
 );
-ALTER TABLE transaction_tags OWNER TO sudhir;
 CREATE TABLE transactions (
     id character varying NOT NULL,
     "timestamp" timestamp without time zone NOT NULL
 );
-ALTER TABLE transactions OWNER TO sudhir;
 ALTER TABLE ONLY lines ALTER COLUMN id SET DEFAULT nextval('lines_id_seq'::regclass);
 ALTER TABLE ONLY account_tags
     ADD CONSTRAINT account_tags_pkey PRIMARY KEY (account_id);
