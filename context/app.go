ce6c447396ca92155458fb548486eecf4a492700
Ilayaraja <ilayaraja.edu@gmail.com>
2017-05-22 09:32:22 +0530

    App initialisation refactor (#2)
    * Established routes handler
    Established http listener
    Made build shell script
    
    * Simple http server for QLedger
    
    * Removed build.sh and config.go
    
    * Removed gorilla/context
    
    * implement accounts api
    
    * use lib/pq for database portability
    
    * populate app context and pass it to handlers
    
    * Return balance of account id requested for
    
    * Intialize return param and assign value instead of using temp variable
    
    * gofmt the code
    
    * read port from env QLEDGER_PORT
    
    * rename env as PORT
    
    * add godep lib/pq
    
    * godep update -goversion
    
    * pass app context as argument to handler
    
    * change SQL case
    
    * move db initialization to main()

--- context/app.go
+++ context/app.go
@@ -2,20 +2,8 @@ package context
 
 import (
 	"database/sql"
-
-	"github.com/RealImage/QLedger/database"
 )
 
 type AppContext struct {
 	DB *sql.DB
-}
-
-func (appContext *AppContext) Initialize() {
-	appContext.DB = database.Dial()
-	// Other contexts to initialize...
-}
-
-func (appContext *AppContext) Cleanup() {
-	appContext.DB.Close()
-	// Other contexts to cleanup...
 }
