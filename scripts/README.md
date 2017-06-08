b235a4c9bfb88d3aca231133ed0d92adba7112c1
Ilayaraja M <ilayaraja@realimage.com>
2017-06-08 12:53:26 +0530

    Add README for scripts

---
+++ scripts/README.md
@@ -0,0 +1,9 @@
+## Scripts
+
+#### Dump schema
+
+```
+cd QLedger
+bash scripts/dump_schema.sql
+cat schema.sql
+```
--- scripts/dump_schema.sh
+++ scripts/dump_schema.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 DB=`echo $DATABASE_URL|cut -d/ -f4|cut -d? -f1`
 pg_dump -s -x -O $DB | grep -v -e "^--" -e "^$" > schema.sql
